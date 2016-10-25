f5d28a8b90f221e7c4d72fc4e4cd978f80799f28
ArthurTlprt <arthur.talpaert@isen-lille.fr>
2016-10-25 13:28:37 +0200

    version finale

--- exe_bezier_ratio.m
+++ exe_bezier_ratio.m
@@ -1,18 +1,65 @@
+
+disp('Click gauche pour insérer les points de contrôle et click droit pour le dernier et terminer.');
+tasto=1;
+figure;
+axis([0 10 0 10 ]);
+grid on;
+hold on;
+
+x=[];y=[];
+%t=input('valeur de t*: ');
+while tasto == 1
+  [xi,yi,tasto]=ginput(1);
+  plot (xi,yi,'r*');
+  x= [x xi];
+  y= [y yi];
+end
+n = length(x);
 % Points de controles
-Px = [1 2 3 4 5 6 7 8];
-Py = [12 10 12 1 12 10 12 10];
+Px = x;
+Py = y;
+
+
+
 
 % Poids
-Poids = [1 10 1 5 1 10 1 10 1];
+Poids = [];
+for i=0:length(Px)-1
+  fprintf('Quel est le poid du point %d?', i)
+  p = input('')
+  Poids(end+1) = p
+end
 
 
 t=0:0.01:1;
 P=zeros(2,length(t));
 % calcul de P pour tous les t € [0, 1]
 for u=1:length(t)
-  P(1, u) = point_bezier_ratio(t(u), Px, Py, Poids)(1)
-  P(2, u) = point_bezier_ratio(t(u), Px, Py, Poids)(2)
+  P(1, u) = point_bezier_ratio(t(u), Px, Py, Poids)(1);
+  P(2, u) = point_bezier_ratio(t(u), Px, Py, Poids)(2);
 end
 
 plot(P(1, 1:end), P(2, 1:end));
-pause(5)
+pause(3)
+
+continuer = (input('Voulez vous modifier un poids?', 's') == 'oui');
+while continuer == 1
+  % Poids
+  Poids = [];
+  for i=0:length(Px)-1
+    fprintf('Quel est le poid du point %d?', i)
+    p = input('')
+    Poids(end+1) = p
+  end
+
+  t=0:0.01:1;
+  P=zeros(2,length(t));
+  % calcul de P pour tous les t € [0, 1]
+  for u=1:length(t)
+    P(1, u) = point_bezier_ratio(t(u), Px, Py, Poids)(1);
+    P(2, u) = point_bezier_ratio(t(u), Px, Py, Poids)(2);
+  end
+  plot(P(1, 1:end), P(2, 1:end));
+  pause(3)
+  continuer = (input('Voulez vous modifier un poids?', 's') == 'oui');
+end
--- point_bezier_ratio.m
+++ point_bezier_ratio.m
@@ -1,31 +1,31 @@
 % Retourne un point P de la courbe de bézier rationelle
 function P = point_bezier_ratio(t, PtControleX, PtControleY, Poids)
-  n = length(PtControleX)
+  n = length(PtControleX);
 
-  Px = 0  % Px correspond à l'abicisse du point P
+  Px = 0;  % Px correspond à l'abicisse du point P
   % calcul du numerateur
   for i=1:n
-    PtControleX(i)
-    Px = Px + Poids(i)*PtControleX(i)*base_bernstein(i-1, n-1, t)
+    PtControleX(i);
+    Px = Px + Poids(i)*PtControleX(i)*base_bernstein(i-1, n-1, t);
   end
   % puis calcul du denominateur
-  denumerateur = 0
+  denumerateur = 0;
   for i=1:n
-    denumerateur = denumerateur + Poids(i)*base_bernstein(i-1, n-1, t)
+    denumerateur = denumerateur + Poids(i)*base_bernstein(i-1, n-1, t);
   end
-  Px = Px/denumerateur
+  Px = Px/denumerateur;
 
-  Py = 0  % Py correspond à l'abicisse du point P
+  Py = 0;  % Py correspond à l'abicisse du point P
   % calcul du numerateur
   for i=1:n
-    Py = Py + Poids(i)*PtControleY(i)*base_bernstein(i-1, n-1, t)
+    Py = Py + Poids(i)*PtControleY(i)*base_bernstein(i-1, n-1, t);
   end
   % puis calcul du denominateur
-  denumerateur = 0
+  denumerateur = 0;
   for i=1:n
-    denumerateur = denumerateur + Poids(i)*base_bernstein(i-1, n-1, t)
+    denumerateur = denumerateur + Poids(i)*base_bernstein(i-1, n-1, t);
   end
-  Py = Py/denumerateur
+  Py = Py/denumerateur;
 
-  P = [Px, Py]
+  P = [Px, Py];
 end
